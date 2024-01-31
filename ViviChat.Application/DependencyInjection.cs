063cf7c0a1d4c38fdcb02af484017c3b40b5a1d1
Imtua <itsololevelling@gmail.com>
2024-01-31 14:48:15 +0300

    Add project files.

---
+++ ViviChat.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace ViviChat.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // TODO: Добавление сервисов слоя Application 
+
+            return services;
+        }
+    }
+}
---
+++ ViviChat.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace ViviChat.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            // TODO: Добавление сервисов EF в коллекцию зависимостей
+
+            return services;
+        }
+    }
+}
